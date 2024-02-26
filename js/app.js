8111b878224932affc286c38c9e6a5fced1f4a20
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-02-26 14:40:28 -0500

    Editing files

--- js/app.js
+++ js/app.js
@@ -1,19 +1,17 @@
-(function () {
-    var map = L.map('map', {
-        zoomSnap: .1,
-        center: [35.233333, -75.530278],
-        zoom: 7,
-        minZoom: 6,
-    });
+var map = L.map('map', {
+    zoomSnap: .1,
+    center: [35.233333, -75.530278],
+    zoom: 7,
+    minZoom: 6,
+});
 
-    // mapbox API parameters
-    const accessToken = `pk.eyJ1IjoibWFya20wMCIsImEiOiJjbHMzbXhmcDAwMDA0MnBucmp0YTI2dHN0In0.Ns7cziMPFHfQp-GBe_MWig`
-    const yourName = 'markm00'
-    const yourMap = 'clsmdvehq042801p21h468tcz'
+// mapbox API parameters
+const accessToken = `pk.eyJ1IjoibWFya20wMCIsImEiOiJjbHMzbXhmcDAwMDA0MnBucmp0YTI2dHN0In0.Ns7cziMPFHfQp-GBe_MWig`
+const yourName = 'markm00'
+const yourMap = 'clsmdvehq042801p21h468tcz'
 
-    // request a mapbox raster tile layer and add to map
-    L.tileLayer(`https://api.mapbox.com/styles/v1/${yourName}/${yourMap}/tiles/256/{z}/{x}/{y}?access_token=${accessToken}`, {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, and <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18
-    }).addTo(map);
-})
+// request a mapbox raster tile layer and add to map
+L.tileLayer(`https://api.mapbox.com/styles/v1/${yourName}/${yourMap}/tiles/256/{z}/{x}/{y}?access_token=${accessToken}`, {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, and <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
