581548898a422c23ed1f0f42adee8e5d86fb45e6
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-02-26 14:27:43 -0500

    Adding mapbox

--- js/app.js
+++ js/app.js
@@ -5,4 +5,15 @@
         zoom: 7,
         minZoom: 6,
     });
+
+    // mapbox API parameters
+    const accessToken = `pk.eyJ1IjoibWFya20wMCIsImEiOiJjbHMzbXhmcDAwMDA0MnBucmp0YTI2dHN0In0.Ns7cziMPFHfQp-GBe_MWig`
+    const yourName = 'markm00'
+    const yourMap = 'clsmdvehq042801p21h468tcz'
+
+    // request a mapbox raster tile layer and add to map
+    L.tileLayer(`https://api.mapbox.com/styles/v1/${yourName}/${yourMap}/tiles/256/{z}/{x}/{y}?access_token=${accessToken}`, {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, and <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+    }).addTo(map);
 })
