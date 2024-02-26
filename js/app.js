c7267766d719e0002a3532ed5eb070d1a9a0a1c3
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-02-26 15:44:39 -0500

    Adding to get icons set up

--- js/app.js
+++ js/app.js
@@ -34,3 +34,48 @@ var hotspots = [{
         icon: '../svg/lighthouse-15.svg',
     }
 }];
+
+// Create empty latLng bounds for zooming/centering map dynamically
+var bounds = L.latLngBounds();
+
+for (var i = 0; i < hotspots.length; i++) {
+    var props = hotspots[i].properties;
+    console.log(props);
+
+    // assign a string, wrapping the name of the place within two HTML bold tags
+    var popup = `<h3>${hotspots[i].name}</h3>
+             <p>${props.description}</p>
+             <p><b>website</b>: <a href='${props.url}'>${props.url}</a></p>
+             `
+
+    popup += props.phone ? `<p><b>phone</b>: ${props.phone}</p>` : "";
+
+    var icon = L.icon({
+        iconUrl: props.icon,
+        iconSize: [40, 40],
+        popupAnchor: [0, -22],
+        className: "icon"
+    });
+
+    var marker = L.marker(hotspots[i].properties.coordinates, {
+        icon: icon
+    })
+        .addTo(map)
+        .bindPopup(popup);
+
+    //  Extend the bounds as features are added
+    bounds.extend(hotspots[i].properties.coordinates)
+
+    marker.on("mouseover", function () {
+        this.openPopup();
+    });
+    marker.on("mouseout", function () {
+        this.closePopup();
+    });
+}
+
+// Fit map to the extent of hotspots
+map.fitBounds(bounds);
+
+// variable to reference the zero index
+var i = 0;
