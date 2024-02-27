0f01698fe1c9ea6cc5d0cd3efdaabe2c0a99c491
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-02-26 19:04:42 -0500

    Adding lighthouses
    Adding other lighthouses from the Outer Banks

--- lighthouses/lighthouse.js
+++ lighthouses/lighthouse.js
@@ -15,6 +15,38 @@ var hotspots = [{
         url: 'https://www.nps.gov/caha/planyourvisit/chls.htm',
         icon: '../svg/lighthouse-15.svg',
     }
+}, {
+    name: "Ocracoke Lighthouse",
+    properties: {
+        location: "Ocracoke Island",
+        coordinates: [35.108982, -75.986032],
+        url: 'https://www.nps.gov/caha/planyourvisit/ols.htm',
+        icon: '../svg/lighthouse-15.svg',
+    }
+}, {
+    name: "Currituck Beach Lighthouse",
+    properties: {
+        location: "Corolla",
+        coordinates: [36.376668, -75.830656],
+        url: 'https://www.visitcurrituck.com/places/currituck-beach-light-house/',
+        icon: '../svg/lighthouse-15.svg',
+    }
+}, {
+    name: "Cape Lookout Lighthouse",
+    properties: {
+        location: "Cape Lookout",
+        coordinates: [34.622742, -76.524529],
+        url: 'https://www.nps.gov/calo/index.htm',
+        icon: '../svg/lighthouse-15.svg',
+    }
+}, {
+    name: "Roanoke Marshes Lighthouse",
+    properties: {
+        location: "Manteo",
+        coordinates: [35.9082116, -75.6682721],
+        url: 'https://www.manteonc.gov/visitors/roanoke-marshes-lighthouse',
+        icon: '../svg/lighthouse-15.svg',
+    }
 }];
 
 // Create empty latLng bounds for zooming/centering map dynamically
