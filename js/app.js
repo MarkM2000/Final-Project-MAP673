82ed72623bd810be3c2fbb45afb72f9a38b150da
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-02-28 19:13:32 -0500

    rmv hotspots as it was moved

--- js/app.js
+++ js/app.js
@@ -16,58 +16,6 @@ L.tileLayer(`https://api.mapbox.com/styles/v1/${yourName}/${yourMap}/tiles/256/{
     maxZoom: 18
 }).addTo(map);
 
-// Hotspots
-/// This hotspot will feature other attractions besides lighthouses.
-var hotspots = [{
-    name: "Wright Brothers National Memorial",
-    properties: {
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/First_Flight_Memorial_5.jpg/256px-First_Flight_Memorial_5.jpg",
-        location: "Kill Devil Hills",
-        coordinates: [36.0143, -75.6679],
-        information: "This granite monument honors the Wright Brothers, who conducted the first successful air flight.",
-        url: 'https://www.nps.gov/wrbr/index.htm',
-        icon: '../svg/monument-15.svg'
-    }
-}]
-
-var bounds = L.latLngBounds();
-
-for (var i = 0; i < hotspots.length; i++) {
-    var props = hotspots[i].properties;
-    console.log(props);
-
-    // assign a string, wrapping the name of the place within two HTML bold tags
-    var popup = `<h3>${hotspots[i].name}</h3>
-    <img src='${props.image}'>
-    <p>${props.location}</p>
-    <p>${props.information}</p>
-    <p><b>URL</b>: <a href='${props.url}'>Link</a></p>
-`
-
-    var icon = L.icon({
-        iconUrl: props.icon,
-        iconSize: [20, 20],
-        popupAnchor: [0, -22],
-        className: "icon"
-    });
-
-    var marker = L.marker(hotspots[i].properties.coordinates, {
-        icon: icon
-    })
-        .addTo(map)
-        .bindPopup(popup);
-
-    //  Extend the bounds as features are added
-    bounds.extend(hotspots[i].properties.coordinates)
-
-    marker.on("mouseover", function () {
-        this.openPopup();
-    });
-    marker.on("mouseout", function () {
-        this.closePopup();
-    });
-}
-
 // Omnivore CSV file 
 omnivore.csv("csv/outer-banks-hotels.csv")
     .on("ready", function (e) {
